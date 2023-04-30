8e0471783b03c286f772a0d1c9fc7653e2ce3e15
MohammdAshik <mohammadashik797@gmail.com>
2023-04-30 20:29:41 +0600

    set initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Root from "./Layout/Root";
+
+function App() {
+  return (
+    <>
+      <Root />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Layout/Root.jsx
@@ -0,0 +1,5 @@
+const Root = () => {
+  return <div>helo</div>;
+};
+
+export default Root;
